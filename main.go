2de8ac89919554e01f3d7dd07748754688cb5ff5
erfan-shams <eshams2003@gmail.com>
2025-01-17 19:52:53 +0330

    Create main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+import (
+"fmt"
+"github.com/erfan-shams/stringmod/quotes"
+"github.com/erfan-shams/stringmod/strings"
+)
+func main() {
+o, e := strings.CountOddEven("12345")
+fmt.Println(o, e) // 3 2
+fmt.Println(quotes.GetEmoji("turtle"))
+}
